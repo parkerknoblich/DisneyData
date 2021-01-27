343d0c50af01276809548da77c2ec0bfdb56e671
DESKTOP-3C06VN5\pckno <pcknoblich@gmail.com>
2021-01-27 13:29:22 -0800

    Added time selector for first park

--- Website/barchart.js
+++ Website/barchart.js
@@ -200,7 +200,7 @@
            labels: currentLands,
            datasets: [
                {
-                   label: "Average Wait Time (mins)",
+                   label: "Predicted Average Wait Time (mins)",
                    data: [30, 20, 55, 30, 25, 15, 35, 40, 5, 0],
                    borderWidth: 2,
                    borderColor: ["#ff0000", "#FFFF00", "#32CD32", "#32CD32", "#32CD32", "#32CD32", "#32CD32", "#32CD32", "#32CD32"],
